1b998171b3d3553e1c2725c0ff0537e1cc45797c
Kibertr <118417803+Kibertr@users.noreply.github.com>
2024-02-29 14:31:56 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -1,220 +1,189 @@
-<head>
-    <style>
-        *{
-            margin: 0px;
-        }
-        .main{
-            width:100%;
-        }
-        .top{
-            margin-top:50px;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            width:100%;
-        }
-        .bot{
-            margin-top:50px;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-        }
-        .left{
-            float:left;
-        }
-        a{
-            color:white;
-            text-decoration:none;
-        }
-        .ya span{
-            color:black;
-        }
-        .ya{
-            padding:5px;
-            background-color:green;
-        }
-        .a{
-            border-radius:20px;
-            background:rgba(0,0,0,0.6);
-            padding:10px;
-            margin:10px;
-        }
-    </style>
-</head>
 <?php
-$m=["1"=>"январь","2"=>"февраль","3"=>"Март","4"=>"Апрель","5"=>"Май","6"=>"Июнь","7"=>"Июль","8"=>"Август","9"=>"Сентябрь","10"=>"Октябрь","11"=>"Ноябрь","12"=>"Декабрь"];
-$mp=["01"=>"январь","02"=>"февраль","03"=>"Март","04"=>"Апрель","05"=>"Май","06"=>"Июнь","07"=>"Июль","08"=>"Август","09"=>"Сентябрь","10"=>"Октябрь","11"=>"Ноябрь","12"=>"Декабрь"];
 class main{
     private $conn = "";
 
-    function __construct(){
-        $this->conn=mysqli_connect("localhost","root","root","school");
+    public function __construct(){
+        $this->conn = mysqli_connect("localhost","root","root",'school');
     }
-    function printGroups(){
-        $sql = "SELECT * FROM groups";
+    function print_groups(){
+        $sql = 'SELECT * FROM groups';
         $result = mysqli_query($this->conn,$sql);
-        while($row = mysqli_fetch_assoc($result)){
-            echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+        while($row = mysqli_fetch_array($result)){
+            if(isset($_GET['group']) && $_GET['group']==$row['id']){
+                echo '<option selected value="'.$row['id'].'">'.$row['name'].'</option>';
+            }else{
+                echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+            }
         }
     }
-    function printSubjects(){
-        $sql = "SELECT * FROM subjects";
+    function print_subjects(){
+        $sql = 'SELECT * FROM subjects';
         $result = mysqli_query($this->conn,$sql);
-        while($row = mysqli_fetch_assoc($result)){
-            echo '<option value="'.$row['subject'].'">'.$row['subject'].'</option>';
+        while($row = mysqli_fetch_array($result)){
+            if(isset($_GET['subject']) && $_GET['subject']==$row['id']){
+                echo '<option selected value="'.$row['id'].'">'.$row['name'].'</option>';
+            }else{
+                echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+            }
         }
     }
-    function printStudents($group){
-        $sql = "SELECT * FROM students where group_id='$group'";
-        $result = mysqli_query($this->conn,$sql);
-        while($row = mysqli_fetch_assoc($result)){
-            echo '<div class="a"><a href="?'.$row['name'].'">'.$row['name'].'</a></div>';
+    function print_table_date($month){
+        $cd = mktime(0,0,0, $month, 1, date('Y',time()));
+        $ld = mktime(0,0,0,$month,date("t",$cd),date('Y',time()));
+        for($i=$cd;$i<=$ld;$i=$i+(3600*24)){
+            echo '<td><div style="10px">'.date('d',$i)."</div></td>";
         }
     }
-    function otmetit($date,$c,$s,$g){
-        $sql ="SELECT * FROM attendance where date='$date' and subject='$s'";
+    function print_month(){
+        $months = ["1"=>"Январь","2"=>"Февраль","3"=>"Март","4"=>"Апрель","5"=>"Май","6"=>"Июнь","7"=>"Июль","8"=>"Август","9"=>"Сентябрь","10"=>"Октябрь","11"=>"Ноябрь","12"=>"Декабрь"];
+        foreach($months as $n=>$t){
+            if(isset($_GET["month"]) && $_GET["month"]==$n){
+                echo '<option selected value="'.$n.'">'.$t.'</otion>';
+            }else{
+                echo '<option value="'.$n.'">'.$t.'</otion>';
+            }
+        }
+    }
+    function print_graph($month,$group,$subject){
+        $sql_s = "SELECT * FROM students where group_id='$group'";
+        $result_s = mysqli_query($this->conn,$sql_s);
+        $mh = 500/mysqli_num_rows($result_s);
+        $sql_at = "SELECT * FROM attendance where month='$month' and group_id='$group' and subject_id='$subject'";
+        $result_at = mysqli_query($this->conn,$sql_at);
+        $cd = mktime(0,0,0, $month, 1, date('Y',time()));
+        $ld = mktime(0,0,0,$month,date("t",$cd),date('Y',time()));
+        $dates = array();
+        while($row = mysqli_fetch_array($result_at)){
+            $dates[$row['date']]= $row['attend'];
+        }
+        for($i=$cd;$i<=$ld;$i=$i+(3600*24)){
+            echo '<td valign="bottom" style="height:500px;text-align:center">';
+                foreach($dates as $d=>$a){
+                if($d==date("Y-m-d",$i)){
+                    $h = $mh*$a;
+                    echo $a.'<div style="height:'.$h.'px;background:green"></div>';
+                }
+            }
+            echo '</td>';
+        }
+    }
+    function add_data($attend,$date,$group,$subject){
+        $sql_s = "SELECT * FROM students where group_id='$group'";
+        $result_s = mysqli_query($this->conn,$sql_s);
+        $total_students = mysqli_num_rows($result_s)+1;
+        $nmonth = explode("-",$date);
+        $month = $nmonth[1];
+        $sql = "INSERT INTO attendance(`date`,`attend`,`group_id`,`month`,`subject_id`,`total_students`) VALUE('$date','$attend','$group','$month','$subject','$total_students')";
         $result = mysqli_query($this->conn,$sql);
-        if(mysqli_num_rows($result)==1){
-            $sqlr="UPDATE attendance set visiting='$c' where subject='$s' and date='$date' and group_id='$g'";
-            $resultr= mysqli_query($this->conn,$sqlr);
+        if($result){
+            echo 'успешно отмечено';
         }else{
-            $sqlr="INSERT INTO `attendance`(`date`, `subject`, `group_id`, `visiting`) VALUES ('$date','$s','$g','$c')";
-            $resultr= mysqli_query($this->conn,$sqlr);
+            echo 'ошибка';
         }
-       
-    }
-    function printSubject(){
-        $sql = "SELECT * FROM subjects";
-        $result = mysqli_query($this->conn,$sql);
-        while($row = mysqli_fetch_assoc($result)){
-            echo '<option value="'.$row['subject'].'">'.$row['subject'].'</option>';
-        }
-    }
-    function printGraf($month,$group,$fd,$year,$subject){
-        $m = date("m",mktime(0,0,0,$month+1,0,$year));
-        for($i=1;$i<=$fd;$i++){
-            echo '<td></td>';
-        }
-        echo '<tr>';
-        for($i=1;$i<=$fd;$i++){
-            $date = $year."-".$m."-".$i;
-            $sql = "SELECT * FROM attendance where date='$date' and subject='$subject'";
-            $result = mysqli_query($this->conn,$sql);
-            if(mysqli_num_rows($result)>0){
-                while($row = mysqli_fetch_assoc($result)){
-                    $c = $row['visiting'];
-                }
-            }else{
-                $c=0;
-            }
-            $sql = "SELECT * FROM students where group_id='$group'";
-            $result = mysqli_query($this->conn,$sql);
-            $d = mysqli_num_rows($result);
-            if($d<1){
-            }else{
-                $e = 50/$d;
-                if($c!=0){
-                    $height=$c*$e;
-                    echo '<td valign="bottom" align="center" width="2%"><div class="ya" style="height:'.$height.'vh;margin-bottom:8px"><span>'.$c.'</span></div></td>';
-                }else{
-                    $height=0;
-                    echo '<td valign="bottom" align="center" width="2%"><div class="ya" style="height:'.$height.'vh;margin-bottom:8px"></div></td>';
-                }
-            }
-        }
-        echo '</tr>';
     }
 }
-$cl=new main();
+$cl = new main();
 ?>
-<div class="main">
-    <div class="container">
-        <div class="top">
-            <div class="left">
-                <form class="form">
-                    отметить учеников<br>
-                    <input type="date" name="date_i"><br>
-                    <select name="group_i">
-                        <?php
-                        $cl->printGroups();
-                        ?>
-                    </select><br>
-                    <input type="number" name="number_i" placeholder="присутствующие"><br>
-                    <select name="subject_i">
-                        <?php
-                        $cl->printSubjects();
-                        ?>
-                    </select><br>
-                    <input type="submit" value="отметить">
-                </form>
-            </div>
-            <div class="left">
-
+<head>
+    <link rel="stylesheet" href="bootstrap.css">
+    <style>
+        .container{
+            display: flex;
+            justify-content: center;
+            margin-top: 100px;
+        }
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        .main{
+            display: flex;
+            flex-direction: column;
+            width:1000px;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <form method="post"class="container row">
+        <div class="col-md-2">
+            <input type="number" name="attend" placeholder="кол-во присутвующих" class="form-control">
+        </div>
+        <div class="col-md-2">
+            <input type="date" name="date" class="form-control">
+        </div>
+        <div class="col-md-2">
+            <select name="group" class="form-control">
+                <?php
+                    $cl->print_groups();
+                ?>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <select name="subject" class="form-control">
+                <?php
+                    $cl->print_subjects();
+                ?>
+            </select>
+        </div>
+        <div style="text-align:center;margin-top:10px;">
+            <div class="col-md-12">
+                <input type="submit" class="btn btn-primary">
             </div>
         </div>
-        <div class="bot">
-            <form>
-                генерация графика
-                <div class="input">
-                    <select name="group">
-                    <?php
-                    $cl->printGroups();
-                    ?>
-                    </select>
-                </div>
-                <select name="month">
-                    <?php
-                    for($i=1;$i<=12;$i++){
-                        echo '<option value="'.$i.'">'.$m[$i].'</option>';
-                    }
-                    ?>
-                </select><br>
-                <select name="subject_i">
-                    <?php
-                        $cl->printSubject();
-                    ?>
-                </select><br>
-                <input type="submit" value="сгенерировать">
-            </form>
-            <table border="1" height='60%'>
-                <td colspan="31">
-                <table width="100%" height="100%">
-                    <?php
-                    if(isset($_GET['month'])){
-                        $month = $_GET['month'];
-                        $group = $_GET['group'];
-                        $year = date("Y",time());
-                        $fd = date("t",mktime(0,0,0,$month+1,0,$year));
-                        $subject = $_GET['subject_i'];
-                        $cl->printGraf($month,$group,$fd,$year,$subject);
-                    }
-                    ?>
-                </table>
-                </td>
-                <tr height="1%">
-                    <?php
-                    if(isset($_GET['month'])){
-                        for($d=1;$d<=$fd;$d++){
-                            echo '<td width="35">'.$d.'</td>';
-                        }
-                    }else{
-                        for($d=1;$d<=31;$d++){
-                            echo '<td width="35">'.$d.'</td>';
-                        }
-                    }
-
+        <input type="hidden" name="post"value="post">
+    </form>
+    <div class="container main">
+        <form class="row">
+            <div class="col-md-3">
+                <select name="month" class="form-control">
+                <?php
+                    $cl->print_month();
+                ?>
+            </select>
+            </div>
+            <div class="col-md-3">
+            <select name="group" class="form-control">
+                <?php
+                    $cl->print_groups();
+                ?>
+            </select>
+            </div>
+            <div class="col-md-3">
+            <select name="subject" class="form-control">
+                <?php
+                    $cl->print_subjects();
+                ?>
+            </select>
+            </div>
+            <div class="col-md-3">
+                <input type="submit" class="btn btn-primary">
+            </div>
+        </form>
+        <div class="div_table">
+            <table class="table" border="1">
+                <tr>
+                <?php 
+                $cl->print_graph($_GET['month'],$_GET['group'],$_GET['subject']);
+                ?>
+                </tr>
+                <tr>
+                <?php
+                $cl->print_table_date($_GET['month']);
                 ?>
                 </tr>
             </table>
         </div>
     </div>
-</div>
+</body>
 <?php
-if(isset($_GET['date_i'])){
-    $date = $_GET['date_i'];
-    $c = $_GET['number_i'];
-    $s = $_GET['subject_i'];
-    $g = $_GET['group_i'];
-    $cl->otmetit($date,$c,$s,$g);
+if(isset($_POST['post'])){
+    if(!empty($_POST['attend']) && !empty($_POST['date']) && !empty($_POST['group']) && !empty($_POST['subject'])){
+        $cl->add_data($_POST['attend'],$_POST['date'],$_POST['group'],$_POST['subject']);
+    }else{
+        echo 'необходимо заполнить все поля';
+    }
 }
 ?>
